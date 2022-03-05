527f33d608b6d3b269dd3dc2881b03f9971c2d1b
Selfie SD <webtamil0@gmail.com>
2022-03-05 06:43:58 +0530

    Update id.py

--- plugins/dm/id.py
+++ plugins/dm/id.py
@@ -48,7 +48,7 @@ button=InlineKeyboardMarkup(
 async def userId(bot, message):
     try:
         await bot.send_chat_action(
-            message.chat.id, "typing..🎑"
+            message.chat.id, "typing"
         )
         if (message.chat.id in BANNED_USERS) or (
             (ADMIN_ONLY) and (message.chat.id not in ADMINS)
